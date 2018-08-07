9073971a71e807578519374b9b890d9b2972569f
tigoe <tom.igoe@gmail.com>
2018-08-07 11:17:37 -0400

    Initial commit

---
+++ ble-ts04.js
@@ -0,0 +1,252 @@
+/*
+This script connects to a TS-04 multimeter using web-bluetooth
+and parses the meter's data protocol. It then calls functions from display.js
+to populate an HTML page.
+
+created 6 Aug 2018
+by Tom Igoe
+*/
+
+
+var myDevice;                         // web-bluetooth peripheral device
+var serviceUuid = 0xffb0;             //UUID of the primary service
+var readingCharacteristic = 0xffb2;   // UUID of the relevant characteristic
+
+// set up a JSON object for the result:
+var meter;
+
+// ------------------ Bluetooth Connection functions
+function connect() {
+  // search for Bluetooth devices: opens the BLE search window:
+  navigator.bluetooth.requestDevice({
+    filters: [{ services: [serviceUuid] }]
+  })
+  .then(function(device) {
+    // save the device returned so you can disconnect later:
+    myDevice = device;
+    // connect to the device once you find it:
+    return device.gatt.connect();
+  })
+  .then(function(server) {
+    // get the primary service:
+    return server.getPrimaryService(serviceUuid);
+  })
+  .then(function(service) {
+    // Step 4: get the meter reading characteristic:
+    return service.getCharacteristic(readingCharacteristic);
+  })
+  .then(function(characteristic) {
+    // subscribe to the characteristic:
+    characteristic.startNotifications()
+    .then(subscribeToChanges);
+  })
+  .catch(function(error) {
+    // catch any errors:
+    console.error('Connection failed!', error);
+  });
+}
+
+// subscribe to changes from the meter:
+function subscribeToChanges(characteristic) {
+  characteristic.oncharacteristicvaluechanged = handleData;
+}
+
+// handle incoming data:
+function handleData(event) {
+  // get the data buffer from the meter:
+  var buf = new Uint8Array(event.target.value.buffer);
+  // decode the results if this is the correct characteristic:
+  if (buf.length == 9) {
+    // decode the binary string:
+    decode(buf);
+    // from display.js, fill the HTML page:
+    fillDisplay(meter);
+  }
+}
+
+// disconnect function:
+function disconnect() {
+  if (myDevice) {
+    // from display.js, clear the HTML page:
+    clearDisplay(meter);
+    // disconnect:
+    myDevice.gatt.disconnect();
+  }
+}
+
+// ------------------ Meter protocol parsing functions
+function decode(data) {
+  meter = {
+    value: '',            // number value
+    negativePolarity: '', // DC negative polarity
+    units: '',            // what you are measuring: V, A, Ω, etc.
+    magnitude: '',        // kilo-, milli-, mega-, micro- ,etc.
+    acDc: '',             // AC or DC, for V and A readings
+    setting: null,        // what setting (function) you're on
+    hold: null,           // hold current reading onscreen
+    autoRange: null,       // autoranging feature
+    ncv: false            // non-contact AC voltage beep
+  }
+  //  debugging the raw data:
+   let reading =''
+   for (let i=0; i< data.length; i++) {
+     reading += data[i].toString(2) + ' ';
+   }
+   console.log(reading);
+
+  /*
+  get the numeric vslues. Each seven-segment LCD numeral is split across
+  two bytes. See http://blog.post for more details
+  */
+
+  //  byte 1 bits 0-1 are AC/DC:
+  if ((data[1] & 0b10) > 0) {
+    meter.acDc = 'DC';
+  } else if ((data[1] & 0b1) > 0) {
+    meter.acDc = 'AC';
+  } else {
+      meter.acDc = '';
+  }
+  // byte 1 but 2 is autoranging:
+  meter.autoRange = ((data[1] & 0b100) > 0);
+
+  // byte 1 bit 4 is the negative sign:
+  if ((data[1] & 0b10000) > 0) {
+    meter.negativePolarity = '-';
+  }
+
+  let readout = '';
+  // iterate over bytes 2-5 to get the values:
+  for (let x=2; x<6; x++) {
+    // the digit value is split across each byte and the one before it:
+    readout += makeDigit(data[x], data[x-1]);
+    // the decimal point for each numeral is in bit 4:
+    if ((x<5) && (data[x] & 0b10000) > 0) {  // decimal point
+      readout += '.';
+    }
+  }
+  // add polarity:
+  if (meter.negativePolarity == false) {
+    meter.value = '-' + meter.value;
+  };
+  meter.value = readout;
+
+  // get units of measurement:
+  // TODO: check for unique values of amperage and battery settings
+  switch (data[7]) {
+    case 0b01001000:    // 72
+    meter.units ='Ω';   // ohms
+    meter.setting = 'Resistance';
+    break;
+    case 0b01001001:    // 73
+    meter.units  = 'A'; // amps
+    meter.setting = 'Amperage';
+    break;
+    case 0b01001010:    // 74
+    meter.units = 'V';  // volts
+    meter.setting = 'Voltage';
+    break;
+    case 0b01011000:    // 88
+    meter.units = '°F'; // degrees Fahrenheit
+    meter.setting = 'Temperature';
+    break;
+    case 0b01101000:    // 104
+    meter.units = '°C'; // degrees Centigrade
+    meter.setting = 'Temperature';
+    break;
+    case 0b11001000:    // 200
+    meter.units = 'NCV';// non-contact AC voltage check
+    meter.setting = 'Non-Contact AC Voltage Check';
+    break;
+  }
+  // get modifiers from data byte 6:
+  // bit 0: milli:
+  if ((data[6] & 0b1) > 0) {
+    meter.magnitude = 'm';
+  }
+  // bit 2: mega
+  if ((data[6] & 0b100) > 0) {
+    meter.magnitude = 'M';
+  }
+  // bit 3: continuity:
+  if ((data[6] & 0b1000) > 0) {
+    meter.setting = 'Continuity';
+    // if the display is 000.0, you've got continuity:
+    if (meter.value === '000.0') {
+      meter.value = 'continuous';
+    }
+  }
+  // Byte 5 bit 6: kilo
+  if ((data[5] & 0b1000000) > 0) {
+    meter.magnitude = 'k';
+  }
+  // bit 7: hold
+  meter.hold = ((data[6] & 0b10000000) > 0);
+
+  // Byte 5 bit 4: micro
+  if ((data[5] & 0b10000) > 0){
+    meter.magnitude = 'µ';
+  }
+  // Byte 5 bit 7: diodeCheck
+  if ((data[5] & 0b10000000) > 0) {
+    meter.setting = 'Diode Check';
+  }
+  // Byte 5 bit 2: NCV beep
+  if (((data[5] & 0b100) > 0) && (meter.setting ==='Non-Contact AC Voltage Check')) {
+    meter.ncv = true;
+  }
+}
+
+// parse the value of a digit from the bits in bytes 1-4
+function makeDigit(byteValue, prevValue) {
+  let digit = '';
+  // combine the upper three bits of the first byte
+  // with the lower four bits of the second byte.
+  // bit 4 is always the decimal point, so it's ignored here:
+  let numberValue = (byteValue & 0b1111) | (prevValue & 0b11100000);
+  switch (numberValue) {
+    case 0:
+    digit = ' ';
+    break;
+    case 0b11101011:
+    digit = '0';
+    break;
+    case 0b1010:
+    digit = '1';
+    break;
+    case 0b10101101:
+    digit = '2';
+    break;
+    case 0b10001111:
+    digit = '3';
+    break;
+    case 0b01001110:
+    digit = '4';
+    break;
+    case 0b011000111:
+    digit = '5';
+    break;
+    case 0b11100111:
+    digit = '6';
+    break;
+    case 0b10001010:
+    digit = '7';
+    break;
+    case 0b11101111:
+    digit = '8';
+    break;
+    case 0b11001111:
+    digit = '9';
+    break;
+    case 0b01100001:
+    digit = 'L';
+    break;
+    case 0b11100101:
+    digit = 'E';
+    break;
+    case 0b11100100:
+    digit = 'F';
+    break;
+  }
+  return digit;
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# General Tools TS-04 Multimeter Browser Interface using Web-Bluetooth
+
+This is a project to create a minimal browser-based interface for the [General Tools TS-04 Multimeter](https://www.generaltools.com/ts04-digitalmultimeter). My intention is to make it possible to build alternative interfaces for the meter for people with a varying range of sensory abilities. Specifically, I want to make a web interface for the mooshimeter that is accessible to low-vision users.
+
+For a detailed Description of the process of deciphering the protocol, see [this blog post](https://www.tigoe.com/pcomp/code/circuits/1309/).
+
+## References:
+
+* [Toolsmart TS04 User Guide](https://www.generaltools.com/productdoc/index/downloadpdf/type/manuals/pdf/TS04-Manual_ES-GT_011416.pdf)
+* [Make: Bluetooth](https://www.makershed.com/products/make-bluetooth) (book) by Alasdair Allan, Don Coleman, and Sandeep Mistry
+* [BLEDocs](https://github.com/tigoe/BLEDocs/wiki), a repository introducing Bluetooth LE
+* [LightBlue](https://itunes.apple.com/us/app/lightblue/id639944780?mt=12), a Bluetooth LE diagnostic app for MacOS by Punchthrough Design
+* The [Web Bluetooth API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API) for JavaScript
+* Yining Shi's [example](https://github.com/yining1023/arduino101CuriePME/tree/master/example1) for Web Bluetooth and p5.js
+* Uri Shaked's [introduction to Web Bluetooth](https://medium.com/@urish/start-building-with-web-bluetooth-and-progressive-web-apps-6534835959a6)
